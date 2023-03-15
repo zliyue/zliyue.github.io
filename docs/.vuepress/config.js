38e1adb926515804d71e5f0e8aa11abe1ad07f84
zhaoliyue <87681169+zliyue@users.noreply.github.com>
2023-03-15 21:50:47 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,28 +24,3 @@ export default defineUserConfig({
     ]
   })
 })
-/*
-export  ({
-  
-  theme: defaultTheme({
-    // 在这里进行配置
-    navbar: [
-      // NavbarItem
-      {
-        text: '首页',
-        link: '/',
-      },
-      // NavbarGroup
-      {
-        text: '前端开发',
-        children: [{
-          text:'HTML&CSS',
-          link:'/frontEndProgramming/HTML&CSS/01_软件开发-发展路线-软件安装-网页组成.md'
-        }],
-      },
-      // 字符串 - 页面文件路径
-      '/bar/README.md',
-    ],
-  }),
-})
-*/
